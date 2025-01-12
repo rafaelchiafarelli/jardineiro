97eee0042d4f205d74c1e7a1474e81e90f236116
Rafael Toschi Chiafarelli <rafel.chiafarelli@gmail.com>
2025-01-12 13:37:40 -0300

    configuration file with passwords and keys

---
+++ src/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID         "SSID"
+#define WIFI_PASS         "PSSWRD"
+#define APP_KEY           "APP_KEY"  //USER DEPENDENT ON SinricPro
+
+#define APP_SECRET        "APP_SECRET"  //USER DEPENDENT ON SinricPro
+
+#define SWITCH_ID_1       "switch Unique number 1"
+#define SWITCH_ID_2       "switch Unique number 2"
+#define SWITCH_ID_3       "switch Unique number 3"
+
+#define BAUD_RATE         115200                // Change baudrate to your need
+
+
+#endif
